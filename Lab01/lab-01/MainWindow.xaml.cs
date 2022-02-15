efb417430db7280eb51cde5a57b8ccc6e936e56d
WladsOnFire <vlad.mishh@gmail.com>
2022-02-15 23:57:29 +0200

    lab01 is done

---
+++ Lab01/lab-01/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab_01
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window1 win1 = new Window1();
+            MainWindow mw = new MainWindow();
+            Visibility = Visibility.Hidden;
+            win1.Show();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Window2 win2 = new Window2();
+            MainWindow mw = new MainWindow();
+            Visibility = Visibility.Hidden;
+            win2.Show();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Window3 win3 = new Window3();
+            MainWindow mw = new MainWindow();
+            Visibility = Visibility.Hidden;
+            win3.Show();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Window4 win4 = new Window4();
+            MainWindow mw = new MainWindow();
+            Visibility = Visibility.Hidden;
+            win4.Show();
+        }
+    }
+}
---
+++ Lab01/lab-01/Window1.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace lab_01
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Visibility = Visibility.Visible;
+            Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                List<string> lines = new List<string>();
+                StreamReader sr = new StreamReader("data.txt");
+                while (!sr.EndOfStream)
+                {
+                    lines.Add(sr.ReadLine());
+                }
+                sr.Close();
+
+                StreamWriter sw = new StreamWriter("data.txt", false, System.Text.Encoding.Default);
+                
+                foreach(string line in lines)
+                {
+                    string[] split = line.Split(' ');
+                    if (split[3] != DN.Text) sw.WriteLine(line);
+                }
+
+                sw.Close();
+                DN.Text = "";
+                DButton.IsEnabled = false;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (DN.Text == "")
+                DButton.IsEnabled = false;
+            else DButton.IsEnabled = true;
+
+            if (Name.Text == "" || Surname.Text == "" || Father.Text == "" || N.Text == "")
+                AButton.IsEnabled = false;
+            else AButton.IsEnabled = true;
+        }
+
+        private void AButton_Click(object sender, RoutedEventArgs e)
+        {
+            StreamWriter sw = new StreamWriter("data.txt", true, System.Text.Encoding.Default);
+            sw.WriteLine(Surname.Text + " " + Name.Text + " " + Father.Text + " " + N.Text);
+            sw.Close();
+            Surname.Text = "";
+            Name.Text = "";
+            Father.Text = "";
+            N.Text = "";
+            AButton.IsEnabled = false;
+        }
+    }
+}
