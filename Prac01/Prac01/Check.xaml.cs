232a9a836d4c5f872490dd5b8f1cefd4a9c12741
WladsOnFire <vlad.mishh@gmail.com>
2022-02-21 15:48:00 +0200

    student done

--- Prac01/Prac01/Check.xaml.cs
+++ Prac01/Prac01/Check.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.IO;
+using System.Diagnostics;
 
 namespace Prac01
 {
@@ -22,6 +24,8 @@ namespace Prac01
         public Check()
         {
             InitializeComponent();
+            StreamReader sr = new StreamReader("keyword.txt");
+            kwL.Content = sr.ReadLine();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -30,5 +34,206 @@ namespace Prac01
             mainWindow.Show();
             Close();
         }
+        static Random rnd = new Random();
+        static int countKeys = 0;
+        static Stopwatch stopWatch = new Stopwatch();
+        static StreamWriter sw2 = new StreamWriter("CheckSample.txt");
+        static int attempts = 0;
+        static int currentAttempt = 0;
+        static double alpha = 0;
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+
+
+            attempts = AttemptsCB.SelectedIndex + 5;
+            switch (AlphaCB.SelectedIndex)
+            {
+                case 0:
+                    alpha = 0.10; //0
+                    break;
+                case 1:
+                    alpha = 0.05; //1
+                    break;
+                case 2:
+                    alpha = 0.01; //2
+                    break;
+                case 3:
+                    alpha = 0.001; //3
+                    break;
+            }
+            e.Handled = true;
+            AlphaCB.IsEnabled = false;
+            AttemptsCB.IsEnabled = false;
+            if (countKeys != kwL.Content.ToString().Length)
+            {
+                if (e.Key.ToString() == "" + kwL.Content.ToString()[countKeys])
+                {
+                    if (countKeys != 0)
+                    {
+                        stopWatch.Stop();
+                        TimeSpan ts = stopWatch.Elapsed;
+                        stopWatch.Reset();
+                        if (countKeys != kwL.Content.ToString().Length - 1)
+                            sw2.Write(ts.Seconds + ts.Milliseconds/1000.0 + " ");
+                        else sw2.Write(ts.Seconds + ts.Milliseconds / 1000.0);
+                    }
+
+                    stopWatch.Start();
+                    CheckTB.Text += e.Key.ToString();
+                    countKeys++;
+                    CheckTB.SelectionStart = CheckTB.Text.Length;
+                }
+            }
+
+            if (countKeys == kwL.Content.ToString().Length)
+            {
+                CheckTB.Text = "";
+                countKeys = 0;
+                sw2.WriteLine();
+                currentAttempt++;
+                stopWatch.Reset();
+                if (currentAttempt == attempts)
+                {
+                    sw2.Close();
+                    CheckTB.IsEnabled = false;
+                    CheckTB.Text = "finished";
+
+
+
+                    List<string> lines = new List<string>();
+                    StreamReader sr2 = new StreamReader("CheckSample.txt");
+                    List<string> Learnlines = new List<string>();
+                    StreamReader sr3 = new StreamReader("sample.txt");
+                    while (!sr2.EndOfStream)
+                    {
+                        lines.Add(sr2.ReadLine());
+                    }
+                    sr2.Close();
+
+                    while (!sr3.EndOfStream)
+                    {
+                        Learnlines.Add(sr3.ReadLine());
+                    }
+                    sr3.Close();
+
+                    List<double[]> attemptArrays = new List<double[]>();
+                    List<double[]> LearnAttemptArrays = new List<double[]>();
+                    
+                    for (int j = 0; j<attempts; j++)
+                    {
+                        double[] array = new double[kwL.Content.ToString().Length - 1];
+                        string[] stringArray = lines[j].Split(' ');
+
+                        for (int i = 0; i < kwL.Content.ToString().Length - 1; i++)
+                            array[i] = double.Parse(stringArray[i]);
+
+                        attemptArrays.Add(array);
+                    }
+
+                    for (int j = 0; j < Learnlines.Count ; j++)
+                    {
+                        double[] array = new double[kwL.Content.ToString().Length - 1];
+                        string[] stringArray = Learnlines[j].Split(' ');
+
+                        for (int i = 0; i < kwL.Content.ToString().Length - 1; i++)
+                            array[i] = double.Parse(stringArray[i]);
+
+                        LearnAttemptArrays.Add(array);
+                    }
+
+                    List<double[]> learnStudentArrays = new List<double[]>();
+                    List<double[]> studentArrays = new List<double[]>();
+                    for (int j = 0; j < Learnlines.Count; j++)
+                    {
+                        double[] stArr = new double[kwL.Content.ToString().Length - 1];
+                        for (int i = 0; i < kwL.Content.ToString().Length - 1; i++)
+                        {
+                            stArr[i] = Student(i, LearnAttemptArrays[j]);
+                        }
+                        learnStudentArrays.Add(stArr);
+                    }
+
+                    for (int j =0; j < attempts; j++)
+                    {
+                        double[] stArr = new double[kwL.Content.ToString().Length - 1];
+                        for (int i = 0; i < kwL.Content.ToString().Length - 1; i++)
+                        {
+                            stArr[i] = Student(i, attemptArrays[j]);
+                        }
+                        studentArrays.Add(stArr);
+                    }
+                    int znachushchist = kwL.Content.ToString().Length - 2;
+
+
+                    StreamWriter sw3 = new StreamWriter("CheckStudent.txt");
+                    foreach(double[] array in studentArrays)
+                    {
+                        for (int i = 0; i < array.Length; i++)
+                        {
+                            if (array[i] > critStudent[znachushchist, AlphaCB.SelectedIndex])
+                                array[i] = 0;
+                            else array[i] = 1;
+                            sw3.Write(array[i] + " ");
+                        }
+                        sw3.WriteLine();
+                    }
+                    sw3.Close();
+
+                    StreamWriter sw4 = new StreamWriter("LearnStudent.txt");
+                    foreach (double[] array in learnStudentArrays)
+                    {
+                        for (int i = 0; i < array.Length; i++)
+                        {
+                            if (array[i] > critStudent[znachushchist, AlphaCB.SelectedIndex])
+                                array[i] = 0;
+                            else array[i] = 1;
+                            sw4.Write(array[i] + " ");
+                        }
+                        sw4.WriteLine();
+                    }
+                    sw4.Close();
+
+                }
+            }
+
+
+        }
+        static double[,] critStudent = new double[9, 4]
+        {  //0.10   0.05  0.01   0.001
+            {6.31, 12.7, 63.7, 637.0},
+            {2.92, 4.3,  9.92, 31.6 },
+            {2.35, 3.18, 5.84, 12.9 },
+            {2.13, 2.78, 4.6,  8.61 },
+            {2.01, 2.57, 4.03, 6.86 },
+            {1.94, 2.45, 3.71, 5.96 },
+            {1.89, 2.36, 3.5,  5.4  },
+            {1.86, 2.31, 3.36, 5.04 },
+            {1.83, 2.26, 3.25, 4.78 },
+        };
+        public double Student(int index, double[]array)
+        {
+            double M = 0;
+            double sum = 0;
+            for(int i = 0; i<array.Length; i++)
+                if (i != index) sum += array[i];
+
+            M = sum / (array.Length - 1.0);
+
+            
+            double sum2 = 0;
+            for (int i = 0; i< array.Length - 1; i++)
+                if (i != index)
+                    sum2 += Math.Pow(array[i] - M, 2);
+
+            double S2 = sum2 / (array.Length - 1.0 - 1.0);
+
+            double S = Math.Sqrt(S2);
+
+            double tp = Math.Abs((array[index] - M) / (S / Math.Sqrt(array.Length - 1)));
+            
+            return tp;
+        }
+
+
     }
 }
--- Prac01/Prac01/Learn.xaml.cs
+++ Prac01/Prac01/Learn.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.IO;
-using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Prac01
 {
@@ -33,14 +33,12 @@ namespace Prac01
             Close();
         }
 
-        static Random rnd = new Random();
-        static bool isLearning = false;
-        static int countKeys = 0;
+  
         private void GenerateBT_Click(object sender, RoutedEventArgs e)
         {
             string alphabet1 = "bcdfghjklmnpqrstvwxz";
             string alphabet2 = "aeiouy";
-            int keyLettersAmmount = rnd.Next(5, 8);
+            int keyLettersAmmount = rnd.Next(6, 10);
             string keyWord = "";
             StreamWriter sw = new StreamWriter("keyword.txt");
 
@@ -49,20 +47,64 @@ namespace Prac01
                 keyWord += "" + alphabet1[rnd.Next(0, 20)];
                 keyWord += "" + alphabet2[rnd.Next(0, 6)];
             }
-                
 
+            keyWord = keyWord.ToUpper();
             KeyWordTB.Text = keyWord;
             sw.WriteLine(keyWord);
             sw.Close();
             LearnKeyWordTB.IsEnabled = true;
         }
-        
-        
+
+        static Stopwatch stopWatch = new Stopwatch();
+        static StreamWriter sw = new StreamWriter("sample.txt");
+        static int attempts = 0;
+        static int currentAttempt = 0;
         private void LearnKeyWordTB_KeyDown(object sender, KeyEventArgs e)
         {
+            attempts = AttemptsCB.SelectedIndex + 5;
             e.Handled = true;
+            GenerateBT.IsEnabled = false;
+            AttemptsCB.IsEnabled = false;
+            if (countKeys != KeyWordTB.Text.Length)
+            {
+                if (e.Key.ToString() == "" + KeyWordTB.Text[countKeys])
+                {
+                    if (countKeys != 0)
+                    {
+                        stopWatch.Stop();
+                        TimeSpan ts = stopWatch.Elapsed;
+                        stopWatch.Reset();
+                        if (countKeys != KeyWordTB.Text.Length-1)
+                            sw.Write(ts.Seconds + ts.Milliseconds / 1000.0 + " ");
+                        else sw.Write(ts.Seconds + ts.Milliseconds / 1000.0);
+                    }
+
+                    stopWatch.Start();
+                    LearnKeyWordTB.Text += e.Key.ToString();
+                    countKeys++;
+                    LearnKeyWordTB.SelectionStart = LearnKeyWordTB.Text.Length;
+                }
+            }
+            if (countKeys == KeyWordTB.Text.Length)
+            {
+                LearnKeyWordTB.Text = "";
+                countKeys = 0;
+                sw.WriteLine();
+                currentAttempt++;
+                stopWatch.Reset();
+                if (currentAttempt == attempts)
+                {
+                    sw.Close();
+                    LearnKeyWordTB.IsEnabled = false;
+                    LearnKeyWordTB.Text = "finished learning";
+                }
+            }
             
-            
+
         }
+
+        static Random rnd = new Random();
+        static int countKeys = 0;
+ 
     }
 }
